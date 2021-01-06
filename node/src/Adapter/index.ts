5a4f822df99893276b3aef87335b6bfcbb3ccb95
satoren <satoreyo@hotmail.com>
2021-01-06 21:34:16 +0900

    feature: Export wasm module path and execute function

--- node/src/Adapter/index.ts
+++ node/src/Adapter/index.ts
@@ -1,2 +1,6 @@
 export { TcpAdapter } from './TcpAdapter'
-export { ChildProcessAdapter } from './ChildProcessAdapter'
+export {
+  ChildProcessAdapter,
+  runLuaAtWasm,
+  debuggableLuaModulePath,
+} from './ChildProcessAdapter'
